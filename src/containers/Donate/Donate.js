fdfbf560baef722a05cf108df4f9cd097c59f85b
AirJunio <arturo@zonet.pl>
2024-01-15 18:41:02 +0100

    Minor Improvements

--- src/containers/Donate/Donate.js
+++ src/containers/Donate/Donate.js
@@ -11,12 +11,23 @@ function Donate(props) {
 
   return (
     <div className='Donate-container'>
+      <div className='column'>
       <Header title={fundraiserData.title} description={fundraiserData.description} newBadge={fundraiserData.newBadge}/>
-      <DonationInfo moneyRaised='US$ 20,712' PageSettings={PageSettings} />
-      <LatestDonations />
       <QuestionsAnswers questionsAndAnswers={fundraiserData.questionsAndAnswers} rewards={fundraiserData.rewards} />
       <QuestionForm />
+      </div>
+      <div className='column'>
+      <DonationInfo moneyRaised='US$ 20,712' PageSettings={PageSettings} />
+      <LatestDonations />
+      </div>
     </div>
+    // <div className='Donate-container'>
+    //   <Header title={fundraiserData.title} description={fundraiserData.description} newBadge={fundraiserData.newBadge}/>
+    //   <DonationInfo moneyRaised='US$ 20,712' PageSettings={PageSettings} />
+    //   <LatestDonations />
+    //   <QuestionsAnswers questionsAndAnswers={fundraiserData.questionsAndAnswers} rewards={fundraiserData.rewards} />
+    //   <QuestionForm />
+    // </div>
   );
 }
 
