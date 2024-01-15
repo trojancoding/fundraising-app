2858ca5540cc95d5d182d65cabd49c27503c33f0
trojancoding <kamiltrojan04@gmail.com>
2024-01-15 18:54:59 +0100

    responsive media

--- src/containers/Donate/Donate.js
+++ src/containers/Donate/Donate.js
@@ -5,29 +5,64 @@ import LatestDonations from '../../components/LatestDonations/LatestDonations';
 import QuestionsAnswers from '../../components/QuestionsAnswers/QuestionsAnswers';
 import QuestionForm from '../../components/QuestionForm/QuestionForm';
 
+import { useState, useEffect } from 'react';
+
+function getWindowDimensions() {
+  const { innerWidth: width, innerHeight: height } = window;
+  return {
+    width,
+    height
+  };
+}
+
+export function useWindowDimensions() {
+  const [windowDimensions, setWindowDimensions] = useState(getWindowDimensions());
+
+  useEffect(() => {
+    function handleResize() {
+      setWindowDimensions(getWindowDimensions());
+    }
+
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
+  return windowDimensions;
+}
+
 function Donate(props) {
   const PageSettings = props.PageSettings;
   const fundraiserData = props.fundraiserData;
+  const { height, width } = useWindowDimensions();
 
   return (
     <div className='Donate-container'>
-      <div className='column'>
-      <Header title={fundraiserData.title} description={fundraiserData.description} newBadge={fundraiserData.newBadge}/>
-      <QuestionsAnswers questionsAndAnswers={fundraiserData.questionsAndAnswers} rewards={fundraiserData.rewards} />
-      <QuestionForm />
-      </div>
-      <div className='column'>
-      <DonationInfo moneyRaised='US$ 20,712' PageSettings={PageSettings} />
-      <LatestDonations />
-      </div>
+      {width <= 1200 &&
+      <>
+            <div className='Donate-container'>
+              <Header title={fundraiserData.title} description={fundraiserData.description} newBadge={fundraiserData.newBadge}/>
+              <DonationInfo moneyRaised='US$ 20,712' PageSettings={PageSettings} />
+              <LatestDonations />
+              <QuestionsAnswers questionsAndAnswers={fundraiserData.questionsAndAnswers} rewards={fundraiserData.rewards} />
+              <QuestionForm />
+            </div>
+      </>
+      }
+      {width > 1200 &&
+      <>
+            <div className='column'>
+            <Header title={fundraiserData.title} description={fundraiserData.description} newBadge={fundraiserData.newBadge}/>
+            <QuestionsAnswers questionsAndAnswers={fundraiserData.questionsAndAnswers} rewards={fundraiserData.rewards} />
+            <QuestionForm />
+            </div>
+            <div className='column'>
+            <DonationInfo moneyRaised='US$ 20,712' PageSettings={PageSettings} />
+            <LatestDonations />
+            </div>
+      </>
+      }
+
     </div>
-    // <div className='Donate-container'>
-    //   <Header title={fundraiserData.title} description={fundraiserData.description} newBadge={fundraiserData.newBadge}/>
-    //   <DonationInfo moneyRaised='US$ 20,712' PageSettings={PageSettings} />
-    //   <LatestDonations />
-    //   <QuestionsAnswers questionsAndAnswers={fundraiserData.questionsAndAnswers} rewards={fundraiserData.rewards} />
-    //   <QuestionForm />
-    // </div>
   );
 }
 
